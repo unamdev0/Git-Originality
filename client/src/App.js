235e7325789d664891fee565ebcf3d7424716408
Udit Namdev <unamdev0@gmail.com>
2019-12-04 23:28:11 +0530

    Client side

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import './style.css'
+import Header from './components/HeaderAndFooter/Header'
+
+function App() {
+  return (
+    <div className="app">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/HeaderAndFooter/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Axios from "axios";
+
+const Header = ()=>{
+    return(
+        <div className="header_wrapper">
+           
+        </div>
+    )
+}
+
+export default Header;
